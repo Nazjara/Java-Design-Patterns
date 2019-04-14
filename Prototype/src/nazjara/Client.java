ca873e575e97a2fe531ca72335cf13343677bea6
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2019-04-15 00:57:56 +0300

    Add Prototype module

---
+++ Prototype/src/nazjara/Client.java
@@ -0,0 +1,15 @@
+package nazjara;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Shape clonedShape1 = ShapeCache.getShape("1");
+        System.out.println("Shape: " + clonedShape1.getType());
+
+        Shape clonedShape2 = ShapeCache.getShape("2");
+        System.out.println("Shape: " + clonedShape2.getType());
+
+        Shape clonedShape3 = ShapeCache.getShape("3");
+        System.out.println("Shape: " + clonedShape3.getType());
+    }
+}
